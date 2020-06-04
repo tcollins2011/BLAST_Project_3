77acd8444fa82699c1a9344ac11a67747592e0c7
Tylre Collins <tyler@nhlp.com>
2020-06-04 15:06:14 -0600

    stash

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -2,11 +2,23 @@ import React, { useState, useEffect } from "react";
 import './home.css'
 
 
-function Home() {   
+function Home() {  
+    const imgMyimageexample = require('../utils/backgroundImages/DNA_Helix.png');
+    const divStyle = {
+    width: '100wh',
+    height: '100vh  ',
+    backgroundImage: `url(${imgMyimageexample})`,
+    backgroundSize: 'cover',
+    opacity: '50%'
+}; 
     return(
-        <div className='DNA'>
-            <h1>hi</h1>
+
+        <div style ={divStyle}>
+            <h1>Look at me</h1>
+            <h2 style={{opacity: '100%'}}>IM here as well</h2>
+    
         </div>
+        
         
     )
 }
